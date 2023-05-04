cf7d28fe16dce20354b71162098129f1892a00fd
Tsubakurouuu <yzpolice2896@icloud.com>
2023-05-04 21:36:01 +0900

    first commit

---
+++ src/main/java/com/example/demo/EclipseGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipseGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipseGithubApplication.class, args);
+	}
+
+}
